74f5fec43404fea30cfda4fc11da434550920724
Joe Cox <joecox@live.co.uk>
2021-03-24 22:53:55 +0000

    added prod and development environment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyD8Wx2ywXK4X8dt6V1oyrbzGT3EQUNOwo8",
-  authDomain: "disney-plus-b0332.firebaseapp.com",
-  databaseURL: "https://disney-plus-b0332-default-rtdb.firebaseio.com",
-  projectId: "disney-plus-b0332",
-  storageBucket: "disney-plus-b0332.appspot.com",
-  messagingSenderId: "85675515126",
-  appId: "1:85675515126:web:4c8508c3563aa404b860d7",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
